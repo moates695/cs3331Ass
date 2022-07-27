d9425779013d12eb9b8c793aa0a579e0c5b70367
Marcus Oates <moates695@gmail.com>
2022-07-27 19:38:27 +1000

    update files

--- helper.py
+++ helper.py
@@ -8,3 +8,14 @@ def checkPortNumber(port):
     if port < 1024:
         print("Error: port number must be 1024 or greater")
         exit()
+
+# split message into Header and Payload
+# with '~' delimeter
+def splitMessage(message):
+    try:
+        header = message.split("~")[0]
+        payload = " ".join(message.split("~")[1:])
+        return header, payload
+    except IndexError:
+        print(f"Error: Message invalid format: {message}")
+        exit()
